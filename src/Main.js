dd4160884a7ffdb6ac64b213deceaf2949940766
HAM\Eric <tunasmoothie@gmail.com>
2020-08-12 10:49:26 +0800

    ''

---
+++ src/Main.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Style.css';
+
+import Button from './components/Button';
+
+function Main() {
+  return (
+    <div className="background">
+      <div className="comp">
+        <header className="comp">
+          <p>
+            Dick hard win hard.
+          </p>
+          <Button />
+        </header>
+      </div>
+      <div align-items="center" justify-content="center">
+        <Button />
+      </div>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p>d</p>
+      <p className="comp">d</p>
+    </div>
+  );
+}
+
+export default Main;
