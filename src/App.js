6ddf524a34635c1bcd3188431a3bfecb336f33fe
HAM\Eric <tunasmoothie@gmail.com>
2020-08-12 09:42:53 +0800

    ''

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import bg_image from './background.jpg';
 import Button from './components/Button';
 
 function App() {
   return (
-    <div className="App" styles={{ backgroundImage:`url(${bg_image}`}}>
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
